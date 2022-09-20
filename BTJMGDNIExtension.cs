bc372b42a79d1bcbc66c8c5ccd7b18e86ca0035e
Brian T. Jackett <brian.jackett@microsoft.com>
2022-09-20 14:16:23 -0400

    Add graphServiceClient to kernal

--- BTJMGDNIExtension.cs
+++ BTJMGDNIExtension.cs
@@ -1,52 +1,61 @@
-﻿using System.CommandLine;
+﻿using Azure.Identity;
+using System;
+using System.CommandLine;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive;
-using Microsoft.DotNet.Interactive.ValueSharing;
-
-using System;
-using Azure.Identity;
+using Microsoft.DotNet.Interactive.CSharp;
+using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.Graph;
 
 namespace BTJMGDNIExtension;
-public class BTJMGDNIKernelExtension : IKernelExtension//, ISupportSetClrValue
+public class BTJMGDNIKernelExtension : IKernelExtension
 {
     private static string SCOPES_STRING = "https://graph.microsoft.com/.default";
-    public static GraphServiceClient _graphServiceClient;
-
+    
     public async Task OnLoadAsync(Kernel kernel)
     {
-        var tenantIdOption = new Option<string>(new[] { "-t", "--tenantId" },
+        if(kernel is not CompositeKernel cs)
+        {
+            return;
+        }
+        var cSharpKernel = cs.ChildKernels.OfType<CSharpKernel>().FirstOrDefault();
+
+        var tenantIdOption = new Option<string>(new[] { "-t", "--tenant-id" },
                                          "Directory (tenant) ID in Azure Active Directory.");
-        var clientIdOption = new Option<string>(new[] { "-c", "--clientId" },
+        var clientIdOption = new Option<string>(new[] { "-c", "--client-id" },
                                          "Application (client) ID registered in Azure Active Directory.");
-        var clientSecretOption = new Option<string>(new[] { "-s", "--clientSecret" },
+        var clientSecretOption = new Option<string>(new[] { "-s", "--client-secret" },
                                          "Application (client) secret registered in Azure Active Directory.");
+        var scopeNameOption = new Option<string>(new[] { "-n", "--scope-name"},
+                                        description: "Scope name for Microsoft Graph connection.", getDefaultValue:() => "graphClient");
 
         var graphCommand = new Command("#!microsoftgraph", "Send Microsoft Graph requests using the specified permission flow.")
         {
             tenantIdOption,
             clientIdOption,
-            clientSecretOption
+            clientSecretOption,
+            scopeNameOption
         };
 
         graphCommand.SetHandler(
-            (string tenantId, string clientId, string clientSecret) =>
+            async (string tenantId, string clientId, string clientSecret, string scopeName) =>
             {
-                _graphServiceClient = GetAuthenticatedGraphClient(tenantId, clientId, clientSecret);
+                var graphServiceClient = GetAuthenticatedGraphClient(tenantId, clientId, clientSecret);
+                await cSharpKernel.SetValueAsync(scopeName, graphServiceClient, typeof(GraphServiceClient));
+                KernelInvocationContextExtensions.Display(KernelInvocationContext.Current, $"Graph client declared with name: {scopeName}");
             }, 
             tenantIdOption,
             clientIdOption,
-            clientSecretOption);
+            clientSecretOption,
+            scopeNameOption);
 
-        kernel.AddDirective(graphCommand);
+        cSharpKernel.AddDirective(graphCommand);
+
+        cSharpKernel.DeferCommand(new SubmitCode("using Microsoft.Graph;"));
 
         return;
     }
- 
-    // public Task SetValueAsync(string name, object value, Type declaredType = null)
-    // {
-    //     return Task.CompletedTask;
-    // }
 
     private static GraphServiceClient GetAuthenticatedGraphClient(string tenantId, string clientId, string clientSecret)
     {
@@ -61,9 +70,9 @@ public class BTJMGDNIKernelExtension : IKernelExtension//, ISupportSetClrValue
         var clientSecretCredential = new ClientSecretCredential(
             tenantId, clientId, clientSecret, options);
 
-        _graphServiceClient = new GraphServiceClient(clientSecretCredential, scopes);
+        var graphServiceClient = new GraphServiceClient(clientSecretCredential, scopes);
 
         Console.WriteLine("Set the Graph client");
-        return _graphServiceClient;
+        return graphServiceClient;
     }
 }
